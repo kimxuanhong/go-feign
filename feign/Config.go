74b558721225e6232451dd462f573abcc736c78a
hong.kim <hong.kim@bstarsolutions.com>
2025-05-23 16:18:30 +0700

    enhance code

--- feign/Config.go
+++ feign/Config.go
@@ -13,16 +13,19 @@ type Config struct {
 	RetryCount int               `mapstructure:"retry_count" yaml:"retry_count"`
 	RetryWait  time.Duration     `mapstructure:"retry_wait" yaml:"retry_wait"`
 	Headers    map[string]string `mapstructure:"headers" yaml:"headers"`
+	Debug      bool              `mapstructure:"debug" yaml:"debug"`
 }
 
 func NewConfig() *Config {
 	timeout, _ := time.ParseDuration(getEnv("FEIGN_TIMEOUT", "30s"))
 	retryWait, _ := time.ParseDuration(getEnv("FEIGN_RETRY_WAIT", "1s"))
 	retryCount, _ := strconv.Atoi(getEnv("FEIGN_RETRY_COUNT", "0"))
+	debug, _ := strconv.ParseBool(getEnv("FEIGN_DEBUG", "false"))
 	return &Config{
 		Timeout:    timeout,
 		RetryCount: retryCount,
 		RetryWait:  retryWait,
+		Debug:      debug,
 	}
 }
 
@@ -41,9 +44,11 @@ func GetConfig(configs ...*Config) *Config {
 	viper.SetDefault("feign.timeout", "30s")
 	viper.SetDefault("feign.retry_count", "0")
 	viper.SetDefault("feign.retry_wait", "1s")
+	viper.SetDefault("feign.debug", false)
 	return &Config{
 		Timeout:    viper.GetDuration("feign.timeout"),
 		RetryCount: viper.GetInt("feign.retry_count"),
 		RetryWait:  viper.GetDuration("feign.retry_wait"),
+		Debug:      viper.GetBool("feign.debug"),
 	}
 }
